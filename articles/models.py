5fbb5d4a4a1011a0c2ff4057972257336d52bd9a
sean <boobookish@gmail.com>
2019-04-23 16:30:25 +0400

    First commit

---
+++ articles/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+
+# Create your models here.
+class AllArticles(Page):
+    description = models.CharField(max_length=255, blank=True)
+
+    control_panels = Page.content_panels + [
+        FieldPanel('description', classname="full")
+    ]
+
+class ArticlePage(Page):
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname='full')
+    ]
---
+++ demo_site/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'tec5z#b$p_em4nuf(f4p3rw!!@z=nw1!1iicq%uta-%&w72f&a'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
