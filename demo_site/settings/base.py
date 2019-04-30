2c54845219dbcb5010d09b562f39c9403cdab1f6
sean <boobookish@gmail.com>
2019-04-30 17:36:49 +0400

    Correct media url

--- demo_site/settings/base.py
+++ demo_site/settings/base.py
@@ -162,7 +162,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '35.236.156.50:8000/media/'
+MEDIA_URL = 'http://35.236.156.50:8000/media/'
 
 
 # Wagtail settings
