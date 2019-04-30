7f8bf5c41dd02ce70eb1b23122121531d9c6ad0e
sean <boobookish@gmail.com>
2019-04-30 17:16:00 +0400

    Add images url to base.py

--- demo_site/urls.py
+++ demo_site/urls.py
@@ -1,3 +1,4 @@
+import os 
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
@@ -39,3 +40,4 @@ if settings.DEBUG:
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static('/media/images/', document_root=os.path.join(settings.MEDIA_ROOT, 'images'))
