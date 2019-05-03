bffc388ffc9c54de7fdcebaa9ea88c90c16c89f3
egvo <gvozdikov@like-coffee.ru>
2019-05-03 13:41:09 +0400

    Вернул urlpatterns для media/images.

--- demo_site/urls.py
+++ demo_site/urls.py
@@ -1,3 +1,5 @@
+import os
+
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
@@ -41,3 +43,4 @@ if settings.DEBUG:
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static('/media/images/', document_root=os.path.join(settings.MEDIA_ROOT, 'images'))
